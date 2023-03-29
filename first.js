a7ebc595f7cab605b4789b1508de15a82be335fd
kes-des <71187027+kes-des@users.noreply.github.com>
2023-03-29 21:20:31 +0530

    First Commit

---
+++ first.js
@@ -0,0 +1,544 @@
+// JAVASCRIPT TUTORIAL TYPE CODE (FROM SCRATCH )
+
+// // variable 
+
+// // let num = 5
+// // console.log (num)
+// //  num=10
+// // console.log(num)
+// //  let username = "gafur bhai"
+// // console.log(username)
+// //  username = "Keshav Don"
+// // console.log (username)
+   
+// // constant 
+
+// // let radius = 2
+// // const pi = 3.14
+// // let area
+// // radius=22
+// // //pi = 34 //(we cannot use it will give error )
+// // area=pi*radius*radius
+// // console.log (area)
+// // //after assing const we cannot reuse the value 
+// // // we can change the value of radius 
+
+// //Data types 
+
+// // let num1=34.44e12
+// // console.log(typeof num1)
+// // //num1=32
+// // let num2=0xfff
+// // let user="fuck"
+// // num2=10_000_00
+// // let num3=-45/0
+// // console.log( Number.MIN_VALUE)
+// // let num1=11223344556677889908888888888888888899n
+// // console.log(num1+)
+// //let num1=11223344556677889908888888888888888899n
+// //console.log(num1+23)//Cannot mix BigInt and other types, use explicit conversion
+// //console.log(num1+23n)
+
+// // dta types string
+
+// // let user = "Navin \n reddy \"\ntrisul\""
+// // console.log(user)
+// // let user1 = 'Navin reddy \"trisul\"'
+// // console.log(user1)
+
+// // let first_name = "Keshav"
+// // let middleName = "kumar"
+// // let $lastname =  "jha"
+// // let username= first_name + " " +middleName + " " +$lastname
+
+// // console.log(username)
+
+// // bool
+
+// // let num = 7<3
+// // console.log(num)
+// // console.log(typeof num) 
+
+// // let user 
+// // console.log(user)
+// // console.log(typeof user)
+
+// // let typ=null
+// // console.log(typ)
+// // console.log(typeof typ)
+
+// // type of conversion
+ 
+// // let num = "123"
+// // console.log(num,  typeof num)
+// // let num1= Number("23")
+// // console.log(num1,  typeof num1)
+
+// // let x 
+// // console.log(x, typeof x)
+
+// // x = 2 + ""
+// // console.log(x, typeof x)
+// //  x =!x
+// //  console.log(x, typeof x)
+
+// //  console.log(Boolean(8))
+// //  console.log(Boolean (0))
+
+// //  console.log(Boolean (null))
+
+// //  console.log(Boolean (undefined))
+
+// // let num = parseInt("123 Keshav")
+// // console.log(num)
+
+// // arithematic operations
+
+// // let num1=true
+// // let num2=false
+// // let result = num1 - num2
+// //  let num = 2
+// // //  ++num
+// // //  num++
+
+// // // let x = ++num
+
+// // let x = num++
+// // console.log (x , num)
+
+// // relational operators 
+// // The following is a list of the JavaScript relational operators.
+
+// // greater-than >
+// // less-than <
+// // greater-than-or-equal-to >=
+// // // less-than-or-equal-to <=
+
+// // let a = 4
+// // let b = 8
+
+// // let x= "1"
+// // let y=2
+// // let result = x>y
+
+// // console.log(result)
+
+// // let x = 1
+// // let y = 1
+// // let result = x=y
+
+// // console.log(result)
+
+// //  let x = "1"
+// // let y = 1
+// // let result = x===y
+
+// //  console.log(result) 
+
+//  // logical operator
+
+// //  let x=2,y=4,t=5
+// //  let result = x>y||y>t // &&
+// //  console.log(result)
+
+
+// // CONDITIONAL IF OR ELSE USING IN JAVASCRIPT
+
+// // let x =2;
+// // let y=9;
+// // let z=7;
+
+// //  let r=x>y;
+// //  if(x>y && x>z){
+// //  console.log("x is greater");
+// //  }
+// //  else if( y>x&&y>z){
+// //  console.log("y is greater");
+// //  }
+// //  else {
+// //     console.log("z is greater")
+// //  }
+
+// //  console.log("bye")
+
+
+// // TERNARY OPERATOR
+
+
+// //   let num = 79
+// //   let result
+// //   if(num%2==0)
+// //   result="even"
+// //   else
+// //       result="odd"
+// //   console.log(result)
+
+// //   let a = 4
+
+// //   let b
+// //   b= a%2==0?"even" : "odd"
+// //   console.log(b)
+
+
+// //   SWITCH CASE 
+
+// //  let month = "April";
+
+// // switch (month) {
+// //     case "January":
+// //        console.log("10pm");
+// //        break;
+// //     case "Feburary":
+// //        console.log("11am"); 
+// //     case " March":
+// //        console.log("12am"); 
+// //     case " April":
+// //        console.log("1pm");
+// //        default:
+// //        console.log("sleep")
+       
+// // }
+// //  console.log("month")
+
+// // TEMPLATE LITERALS IN JAVA SCRIPT
+
+// // let num1 = 12
+// // let num2 = 14
+// // let result = num1+num2
+// // console.log(`The addition of ${num1} and  ${num2} is ${result}`)
+// // console.log("My name is\n KESHAV \n KUMAR\n JHA")
+
+// // while loop 
+
+// // let i =5
+// // while(i>=0)
+// // {
+// //     console.log("hi",i)
+// //     i--
+
+// // }
+
+// // Do while
+// // let i= 10
+// // do{
+// //     console.log("Hi" ,i)
+// //     i++
+
+// // }while(i<=5)
+
+// // for loop 
+
+// for(i=1;i<=5;i++)
+// {
+//     console.log("Hi",i);
+//     for(j=1;j<=5;j++)
+//     console.log("Hello",j);
+// }
+
+// While loop Vs For loop
+
+// for(i=1;i<=100;i++)
+//  {
+//     if(i%3==0)
+//     console.log("Hi",i);
+//  }
+
+//  now while loop 
+
+// let num = 123456
+
+// console.log(num%10);
+// num=parseInt(num/10)
+// console.log(num)
+// console.log(num%10);
+// num=parseInt(num/10)
+// console.log(num)
+// console.log(num%10);
+// num=parseInt(num/10)
+// console.log(num)
+// console.log(num%10);
+// num=parseInt(num/10)
+// console.log(num)
+// console.log(num%10);
+// num=parseInt(num/10)
+// console.log(num)
+
+// Easy Solutio to do this
+
+// let num= 123456
+// while(num>0){
+//     console.log(num%10)
+//     num=parseInt(num/10)
+//     num=parseInt(num/10)
+// // console.log(num)
+// }
+
+
+    // OBJECTS IN JS
+    // let input ="name"
+    // let alien ={
+    //     name:'Keshav',
+    //     exp:2,
+    //     tech:'JS'
+    // }
+    // console.log(alien['exp','tech'])
+
+    // complex objects annd delete
+
+    
+    //  let alien ={
+    //     name:'Keshav',
+    //     exp:2,
+    //     tech:'JS',
+    //     laptop:{
+    //         brand:'hp',
+
+
+
+    //     }
+    //  }
+
+    //  delete alien.tech
+    //  //console.log(alien.laptop.brand.length)
+    //  console.log(alien)
+
+    // use for loop in objects
+
+    // let alien ={
+    //     name:'Keshav',
+    //     exp:2,
+    //     tech:'JS',
+    //     laptop:{
+    //         brand:'hp',
+
+
+
+    //     }
+    //  }
+
+    //  for(let key in alien.laptop)
+    //  {
+    //     console.log(key)
+    //  }
+
+    //  Calling Function 
+
+    // function greet(){
+    //     console.log("Hello World")
+    // }
+
+    // greet()
+    // console.log("1")
+    // greet()
+    // greet()
+
+    // Return value in function 
+
+    // function greet()
+
+    // {
+    //     return "HEY"
+    // }
+    // let str=greet()
+    // console.log(str)
+
+    // Passing a value in function 
+
+    // function greet(user)
+
+    // {
+    //     return `HEY ${user}`
+    // }
+    // let user='Name'
+    // let str=greet(user)
+    // console.log(str)
+
+
+    //function expression in js
+
+//     let add = function (num1,num2)
+
+// {
+//     return num1+num2
+// }
+
+// let sum=add(5,6)
+// console.log(sum)
+
+// ARROW FUNCTION 
+// let greet = (user)=>
+// {
+
+//     console.log("hey"+ " "+ user)
+//     return 1
+// }
+
+// console.log(greet('bb'))
+
+// let add=(num1,num2)=>num1+num2
+// let result = add(5,-6)
+// console.log(result)
+
+// Methods in Js
+
+//   
+// laptop.greet() 
+
+// THIS IN JS
+// let laptop={
+//     name:"cpu",
+//     range:20,
+
+//     getconfig:function(){
+//         console.log(this.name)
+//     }
+// }
+
+// laptop.getconfig()
+
+
+// Constructor 
+
+// function Alien(name,tech){
+//     this.name=name;
+//     this.tech=tech;
+
+// }
+
+// let alien1=new Alien("Keshav","JS");
+// let alien2=new Alien("Kiran","Wife");
+// alien1.tech='blockchain';
+// console.log(alien1)
+
+
+//  Creating Arrays 
+
+// let values=[2,3,4,5,6]
+// // values.push(5)
+// console.log(values[2])
+
+// Different types of array
+
+// let values=["Kiran","Paveen","Ayush"]
+// // values.push("Rahim")
+// values[2]="suraj"
+// console.log(values)
+
+// Different Methods In Array
+// let data=[4,6,7,8,9]
+// console.log(data.push(5))
+// console.log(data)
+// let data=[4,6,7,8,9]
+// // console.log(data.push(5))
+// // console.log(data.pop())
+// // console.log(data.unshift(5))
+// console.log(data.splice(2,3,11,15))
+// console.log(data)
+
+
+// for of loops
+
+
+// let  nums =[]
+// nums[0]=5
+// nums[99]=9
+// for( let n of nums){
+// console.log(n)
+// }
+
+// Array  Destruction in Js
+
+// let nums=[1.2,3,4,5]
+// console.log(nums)
+
+// suppose we want to fetch 1 element 
+
+// let nums=[1,2,3,4,]
+// console.log(nums)
+// let [a,b,,d]=nums
+// console.log(d)
+
+// swapping
+
+// let a=6
+// let b=9
+// [a,b]=[b,a]
+// console.log(a,b)
+
+// let words = "My name is Keshav kumar jha KKJ".split(' ')
+// // let [a,b,c,d]=words
+// let [a,b,,...d]=words
+// console.log(d)
+ 
+
+// array methods JS
+// let array =[12,23,45,67,89,80]
+// array.forEach(n => {
+//     console.log(n)
+// });
+// array.forEach((n,i,array) => {
+//     console.log(n,i,array)
+// });
+
+// filter in array
+
+//   
+
+// let array1=[1,2,3,4,5,6]
+// let result = array1.filter(n=>n%2==0)
+//   .map(n=>n*2)
+//   .reduce((a,b)=> a+b);
+//   console.log(result)
+
+// Set in array 
+//  let letter =  new Set ("aman")
+
+//  letter.add(3)
+//  letter.add(4)
+//  letter.add(3)
+//  letter.add(2)
+//  letter.add("tushar")
+//  letter.add(3)
+
+// //  letter.forEach(n => {
+// //     console.log(n)
+    
+// //  });
+
+// console.log(letter.has("Tushar"))
+
+// map in arrays 
+
+// let map=new Map()
+// map.set("Riya","st")
+// map.set("Naveen","sd")
+// map.set("Keshav","Tc")
+// map.set("Keshav","hhy")
+// // console.log(map.has("Keshav"))
+// // console.log(map.get("Keshav"))
+
+// for(let [k ,v ] of map){
+//     console.log(k, " : " ,v)
+// }
+
+// Recursion in JS
+// let num = 1
+// function show(){
+// console.log("Hi",num)
+// num++
+// if(num<=10351)
+// show()
+// }
+// show()
+
+// factorial
+
+// function fact(n){
+//     if(n==0)
+//        return 1
+//     else 
+//       return n*(n-1)
+// }
+// let num =171
+// let result = fact(num)
+// console.log(result)
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+let array =[12,23,45,67,89,80]
+// array.filter(n=>n%2==0)
+//      .map(n=>n*8)
+//      .forEach(n => {
+//     console.log(n)
+// });
